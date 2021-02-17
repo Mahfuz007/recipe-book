5e04cea9bdcf668e088a471078f10eb871adf37d
mahfuz007 <aamahfuz2@gmail.com>
2021-02-17 08:51:33 +0600

    added dropdown directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipesListComponent } from './recipes/recipes-list/recipes-list.compon
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
+import { DropdownDirective } from './Shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipe
     RecipesListComponent,
     RecipeDetailsComponent,
     ShoppingListEditComponent,
-    RecipesItemComponent
+    RecipesItemComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
